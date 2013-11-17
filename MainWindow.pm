2ddadd9c4c1ac97626bb84c3ad5079690d380940
Andrej Vinokurov <samogon@web.de>
2013-11-17 22:38:55 +0100

    init main window

---
+++ MainWindow.pm
@@ -0,0 +1,17 @@
+package MainWindow;
+
+use strict;
+use warnings;
+
+use 5.010;
+use Data::Dumper;
+
+use QtCore4;
+use QtGui4;
+use QtCore4::isa qw( Qt::MainWindow );
+
+sub NEW {
+   shift->SUPER::NEW( @_ ); # wtf????
+}
+
+1;
