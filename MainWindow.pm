2b722395d2aeb1bbd4ba2c46f535c568dcd5558c
Andrej Vinokurov <samogon@web.de>
2013-11-18 23:19:50 +0100

    add file menu and exit with ctrl+q

--- MainWindow.pm
+++ MainWindow.pm
@@ -10,8 +10,30 @@ use QtCore4;
 use QtGui4;
 use QtCore4::isa qw( Qt::MainWindow );
 
+sub getExitAct() {
+    return this->{exitAct};
+}
+
 sub NEW {
-   shift->SUPER::NEW( @_ );
+    my ( $class ) = @_;
+    $class->SUPER::NEW();
+    my $widget = Qt::Widget();
+    this->setCentralWidget($widget);
+
+    this->createActions();
+    this->createMenus();
+}
+
+sub createActions() {
+    my $exitAct = this->{exitAct} = Qt::Action(this->tr('E&xit'), this);
+    $exitAct->setShortcut(Qt::KeySequence(this->tr('Ctrl+Q')));
+    $exitAct->setStatusTip(this->tr('Exit the application'));
+    this->connect($exitAct, SIGNAL 'triggered()', this, SLOT 'close()');
+}
+
+sub createMenus {
+    my $fileMenu = this->{fileMenu} = this->menuBar()->addMenu(this->tr('&File'));
+    $fileMenu->addAction(this->getExitAct);
 }
 
 1;
