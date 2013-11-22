1eaf0c8ec939584d2ddda8d10453b67849530410
Andrej Vinokurov <samogon@web.de>
2013-11-23 00:36:47 +0100

    initial add of choose character race

--- CentralWidget.pm
+++ CentralWidget.pm
@@ -16,13 +16,33 @@ sub NEW {
 
     my $sex = Qt::ComboBox();
     this->{sexComboBox} = $sex;
+
     $sex->addItem( this->tr('Female') );
     $sex->addItem( this->tr('Male') );
     $sex->addItem( this->tr('Neuter') );
+
     $sex->setToolTip( this->tr("Your 'sex' does not have any significant gameplay effects") );
+
+    my $race = Qt::ComboBox();
+    this->{raceComboBox} = $race;
+
+    $race->addItem( this->tr('Human') );
+    $race->addItem( this->tr('Half-elf') );
+    $race->addItem( this->tr('Elf') );
+    $race->addItem( this->tr('Hobbit') );
+    $race->addItem( this->tr('Gnome') );
+    $race->addItem( this->tr('Dwarf') );
+    $race->addItem( this->tr('Half-Orc') );
+    $race->addItem( this->tr('Half-Troll') );
+    $race->addItem( this->tr('Dunadan') );
+    $race->addItem( this->tr('High-Elf') );
+    $race->addItem( this->tr('Kobold') );
+
+    $race->setToolTip( this->tr("Your 'race' determines various intrinsic factors and bonuses") );
 
     my $layout = Qt::GridLayout();
     $layout->addWidget( this->{sexComboBox}, 0, 0 );
+    $layout->addWidget( this->{raceComboBox}, 1, 0 );
 
     this->setLayout( $layout );
 }
