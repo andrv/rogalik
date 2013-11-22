665de8a977a8a5c97b32718e89edcfcf184418bf
Andrej Vinokurov <samogon@web.de>
2013-11-22 23:57:11 +0100

    initial add of choose character sex

--- CentralWidget.pm
+++ CentralWidget.pm
@@ -13,6 +13,17 @@ use QtCore4::isa qw( Qt::Widget );
 sub NEW {
     my ( $class ) = @_;
     $class->SUPER::NEW();
+
+    my $sex = Qt::ComboBox();
+    this->{sexComboBox} = $sex;
+    $sex->addItem( this->tr('Female') );
+    $sex->addItem( this->tr('Male') );
+    $sex->addItem( this->tr('Neuter') );
+
+    my $layout = Qt::GridLayout();
+    $layout->addWidget( this->{sexComboBox}, 0, 0 );
+
+    this->setLayout( $layout );
 }
 
 1;
