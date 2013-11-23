0924a72606c62b88add96bc5d7e46d0001334101
Andrej Vinokurov <samogon@web.de>
2013-11-23 23:49:50 +0100

    update character basic factors skeleton

--- CentralWidget.pm
+++ CentralWidget.pm
@@ -40,13 +40,44 @@ sub NEW {
 
     $race->setToolTip( this->tr("Your 'race' determines various intrinsic factors and bonuses") );
 
+    my $charFactors = Qt::GridLayout();
+    this->{charFactorsLayout} = $charFactors;
+
+    my $charBasicFactors = Qt::GridLayout();
+    this->{charBasicFactorsLayout} = $charBasicFactors;
+
     my $strengthLabel = Qt::Label( this->tr('Str:') );
     this->{strengthLabel} = $strengthLabel;
+
+    my $strengthData = Qt::Label( this->tr('data') );
+    this->{strengthDataLabel} = $strengthData;
+
+    my $dexterityLabel = Qt::Label( this->tr('Dex:') );
+    this->{dexterityLabel} = $dexterityLabel;
+
+    my $dexterityData = Qt::Label( this->tr('data') );
+    this->{dexterityDataLabel} = $dexterityData;
+
+    my $intelligenceLabel = Qt::Label( this->tr('Int:') );
+    this->{intelligenceLabel} = $intelligenceLabel;
+
+    my $intelligenceData = Qt::Label( this->tr('data') );
+    this->{intelligenceDataLabel} = $intelligenceData;
+
+    $charBasicFactors->addWidget( this->{strengthLabel}, 0, 0 );
+    $charBasicFactors->addWidget( this->{strengthDataLabel}, 0, 1 );
+    $charBasicFactors->addWidget( this->{dexterityLabel}, 0, 2 );
+    $charBasicFactors->addWidget( this->{dexterityDataLabel}, 0, 3 );
+    $charBasicFactors->addWidget( this->{intelligenceLabel}, 1, 0 );
+    $charBasicFactors->addWidget( this->{intelligenceDataLabel}, 1, 1 );
+
+    $charFactors->addLayout( this->{charBasicFactorsLayout}, 0, 0 );
 
     my $layout = Qt::GridLayout();
     $layout->addWidget( this->{sexComboBox}, 0, 0 );
     $layout->addWidget( this->{raceComboBox}, 1, 0 );
-    $layout->addWidget( this->{strengthLabel}, 1, 1 );
+
+    $layout->addLayout( this->{charFactorsLayout}, 1, 1 );
 
     this->setLayout( $layout );
 }
