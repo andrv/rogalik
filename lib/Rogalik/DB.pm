1801d881154fdd6211fdc074cbd79d84a0b899a0
Andrej Vinokurov <samogon@web.de>
2013-11-28 23:43:24 +0100

    add param to setupDb

--- lib/Rogalik/DB.pm
+++ lib/Rogalik/DB.pm
@@ -6,7 +6,11 @@ use warnings;
 use 5.010;
 
 sub setupDb {
-    #
+    # allows Rogalik::DB->setupDb() syntax
+    my $class = ref $_[0] ? ref shift : shift;
+
+    my $databaseFile = shift;
+    say $databaseFile;
 }
 
 1;
