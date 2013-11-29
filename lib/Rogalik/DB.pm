0b93fdc22f4e6bf2e99aa3ca2adef9b8a9ba5fce
Andrej Vinokurov <samogon@web.de>
2013-11-29 16:13:15 +0100

    refactoring

--- lib/Rogalik/DB.pm
+++ lib/Rogalik/DB.pm
@@ -4,13 +4,15 @@ use strict;
 use warnings;
 
 use 5.010;
+use lib '../lib';
+use Rogalik::Config;
 
 sub setupDb {
     # allows Rogalik::DB->setupDb() syntax
     my $class = ref $_[0] ? ref shift : shift;
 
-    my $databaseFile = shift;
-    say $databaseFile;
+    my $dbfile = shift;
+    say "\$dbfile: $dbfile";
 }
 
 1;
