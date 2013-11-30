8c0c02f3a6198016aaf24c2b028a4979d27f6d45
Andrej Vinokurov <samogon@web.de>
2013-12-01 00:51:01 +0100

    add connect(), non-select function to execute() and first table

--- lib/Rogalik/DB.pm
+++ lib/Rogalik/DB.pm
@@ -10,29 +10,64 @@ use DBI;
 use lib '../lib';
 use Rogalik::Config;
 
-sub setupDb {
+my $dbh;
+
+sub connect {
     # allows Rogalik::DB->setupDb() syntax
     my $self = ref $_[0] ? ref shift : shift;
 
+    $dbh = DBI->connect( 'dbi:SQLite:dbname='.Rogalik::Config::DBFILE, "", "" ) unless $dbh;
+
+    return $dbh;
+}
+
+sub setupDb {
+    my $self = ref $_[0] ? ref shift : shift;
+
     my $dbfile = shift;
-#    say "\$dbfile: $dbfile";
 
 #    my @driver_names = DBI->available_drivers;
-#    print Dumper \@driver_names;
 
-    my $dbh = DBI->connect( "dbi:SQLite:dbname=$dbfile", "", "" );
 #    my %drivers = DBI->installed_drivers;
-#    print Dumper \%drivers;
 
-    my $sql = '';
-    my( $result, $rc, $rv ) = $self->execute( $sql );
+    my $sql = 'CREATE TABLE IF NOT EXISTS sex (
+        id      INTEGER,
+        guiId   INTEGER,
+        guiText TEXT,
+        PRIMARY KEY( id ASC ) )';
+    my( $result, $rv ) = $self->execute( $sql );
+
+    say "$result, $rv";
 }
 
 sub execute {
     my $self = ref $_[0] ? ref shift : shift;
 
     my $sql = shift;
-    print Dumper $sql;
+
+    my $dbh = $self->connect;
+
+    unless( $sql =~ m/^select/i ) {
+        $dbh->do( $sql );
+
+        my $rc = $dbh->err ?
+            $dbh->err :
+            0;
+
+        my $rv = $dbh->err ?
+            $dbh->errstr :
+            'successful';
+
+        return ( $sql, $rv, $rc );
+#        say '$dbh->err: '.$dbh->err;
+#        say '$dbh->errstr: '.$dbh->errstr;
+#        say '$dbh->state: '.$dbh->state;
+#DBD::SQLite::db do failed: table sex has no column named x at lib/Rogalik/DB.pm line 51.
+# CREATE TABLE
+#$dbh->err: 1
+#$dbh->errstr: table sex has no column named x
+#$dbh->state: S1000
+    }
 }
 
 1;
