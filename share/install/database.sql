8431cd8cd3ab03d12ecd53bb1477c443bf74d713
Andrej Vinokurov <samogon@web.de>
2013-12-05 22:03:23 +0100

    resolve insert conflicts

--- share/install/database.sql
+++ share/install/database.sql
@@ -26,15 +26,15 @@ create table if not exists race (
   primary key( guiId asc )
 );
 
-insert into race (
+replace into race (
   guiId, name, strenght, dexterity, intelligence, constitution, wisdom, charisma, hit, shoot, throw, hitDie, XPmod, disarm, devices, save, stealth, infravision, diggig, search
 ) values ( 1, 'Human', 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 100, 0, 0, 0, 0, 0, 0, '+0/10');
-insert into race (
+replace into race (
   guiId, name, strenght, dexterity, intelligence, constitution, wisdom, charisma, hit, shoot, throw, hitDie, XPmod, disarm, devices, save, stealth, infravision, diggig, search, sustains )
 values ( 2, 'Half-Elf', 0, 1, 1, -1, -1, 1, -1, 5, 5, 10, 110, 2, 3, 3, 1, 20, 0, '+6/11', 'dexterity' );
-insert into race (
+replace into race (
   guiId, name, strenght, dexterity, intelligence, constitution, wisdom, charisma, hit, shoot, throw, hitDie, XPmod, disarm, devices, save, stealth, infravision, diggig, search, sustains, resists )
 values ( 3, 'Elf', -1, 1, 2, -2, -1, 1, -5, 15, 15, 9, 120, 5, 6, 6, 3, 30, 0, '+8/12', 'dexterity', 'light damage' );
-insert into race (
+replace into race (
   guiId, name, strenght, dexterity, intelligence, constitution, wisdom, charisma, hit, shoot, throw, hitDie, XPmod, disarm, devices, save, stealth, infravision, diggig, search, sustains, extra )
 values ( 4, 'Hobbit', -2, 3, 2, +2, 1, 1, -10, 20, 20, 7, 110, 15, 18, 18, 4, 40, 0, '+12/15', 'experience', 'Identifies mushrooms' );
