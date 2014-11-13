0e3f51218c4e6b1a059a0ede9b18329d6d1d6c3a
Andrej Vinokurov <samogon@web.de>
2014-11-13 14:50:12 +0100

    add character choice

--- ChooseCharacter.pm
+++ ChooseCharacter.pm
@@ -20,11 +20,46 @@ sub NEW {
 
     this->setWindowTitle( 'Choose character' );
 
+    this->{charactersComboBox} = this->createCombo( @characterIds );
+
     # main layout
     my $layout = Qt::GridLayout( this );
 
     # left placeholder
     $layout->setColumnMinimumWidth( 0, 20 );
+
+    $layout->addWidget(
+        Qt::Label( this->tr( 'Non dead character exists'.
+                "\n".
+                'Please choose one:'
+        ) ),
+        0, 1,
+#        1, 3,
+#        Qt::AlignHCenter() | Qt::AlignBottom(),
+    );
+
+    $layout->addWidget( this->characters(), 1, 1 );
 }
+
+sub createCombo {
+    my @characterIds = @_;
+
+    my $combo = Qt::ComboBox();
+
+    foreach my $id( @characterIds ) {
+        my( $res, $rows, $rv ) = Rogalik::DB->execute( "select name, sex, race, class, updated from theCharacter where id = $id" );
+        my $char = $res->[0];
+        my $name = $char->{name} || 'No name yet';
+
+        $combo->addItem( this->tr( "$name, $char->{sex}, $char->{race}, $char->{class}, $char->{updated}" ) );
+    }
+
+    $combo->addItem( this->tr( 'Add new one' ) );
+
+    return $combo;
+}
+
+# getters
+sub characters { return this->{charactersComboBox} }
 
 1;
