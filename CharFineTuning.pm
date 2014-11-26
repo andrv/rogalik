44dde5bb11422f782f13a76bb3ec632b364f915c
Andrej Vinokurov <samogon@web.de>
2014-11-26 22:40:14 +0100

    adjust char fine tuning layout to angband 3.5.0, correct comments

--- CharFineTuning.pm
+++ CharFineTuning.pm
@@ -24,7 +24,7 @@ sub NEW {
     # main layout
     my $layout = Qt::GridLayout( this );
 
-    # left placeholder
+    # left spacer
     $layout->setColumnMinimumWidth( 0, 20 );
     # free space at top
     $layout->setRowMinimumHeight( 0, 20 );
@@ -44,12 +44,12 @@ sub NEW {
 
     $layout->addWidget( this->basicsOne( $characterId ), 2, 1 );
 
-    # empty space
+    # spacer
     $layout->setColumnMinimumWidth( 2, 20 );
 
     $layout->addWidget( this->basicsTwo(), 2, 3 );
 
-    # empty space
+    # spacer
     $layout->setColumnMinimumWidth( 4, 20 );
 
     $layout->addWidget( this->qualities(), 2, 5 );
@@ -61,17 +61,17 @@ sub NEW {
     $layout->addWidget( this->detailedTwo(),   4, 3 );
     $layout->addWidget( this->detailedThree(), 4, 5 );
 
-    # space between middle and bottom
+    # spacer between middle and bottom
     $layout->setRowMinimumHeight( 5, 20 );
 
     this->setCharTxtInfo( Qt::Label( this->tr( '(tbd) You are one of several children of a Yeoman. You are the black sheep of the family. You have dark brown eyes, curly blond hair, and a very dark complexion. (tbd) Bla, bla, bla.' ) ) );
     this->charTxtInfo()->setWordWrap( 1 );
     $layout->addWidget( this->charTxtInfo(), 6, 1, 1, 5, Qt::AlignTop() );
 
-    # space after bottom
+    # spacer after bottom
     $layout->setRowMinimumHeight( 7, 20 );
 
-    # space at right edge
+    # spacer at right edge
     $layout->setColumnMinimumWidth( 6, 20 );
 }
 
@@ -107,19 +107,20 @@ sub basicsOne {
 sub basicsTwo {
     my $table = Qt::GridLayout();
 
-    $table->addWidget( Qt::Label( this->tr( '' ) ),             0, 0 ); # placeholder
-    $table->addWidget( Qt::Label( this->tr( 'Age' ) ),          1, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Height' ) ),       2, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Weight' ) ),       3, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Social' ) ),       4, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Maximize' ) ),     5, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Age' ) ),          0, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Height' ) ),       1, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Weight' ) ),       2, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Turns used:' ) ),  3, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Game' ) ),         4, 0, Qt::AlignRight() );
+    $table->addWidget( Qt::Label( this->tr( 'Standard' ) ),     5, 0, Qt::AlignRight() );
+    $table->addWidget( Qt::Label( this->tr( 'Resting' ) ),      6, 0, Qt::AlignRight() );
 
-    $table->addWidget( Qt::Label( this->tr( '19 (tbd)' ) ),     1, 1 );
-    $table->addWidget( Qt::Label( this->tr( '71 (tbd)' ) ),     2, 1 );
-    $table->addWidget( Qt::Label( this->tr( '139 (tbd)' ) ),    3, 1 );
-    $table->addWidget( Qt::Label( this->tr( 'Pariah (tbd)' ) ), 4, 1 );
-    $table->addWidget( Qt::Label( this->tr( 'Yes (tbd)' ) ),    5, 1 );
-    $table->addWidget( Qt::Label( this->tr( '' ) ),             6, 1 ); # placeholder
+    $table->addWidget( Qt::Label( this->tr( '50 (tbd)' ) ),      0, 1 );
+    $table->addWidget( Qt::Label( this->tr( q/3'8" (tbd)/ ) ),   1, 1 );
+    $table->addWidget( Qt::Label( this->tr( '9st 8lb (tbd)' ) ), 2, 1 );
+    $table->addWidget( Qt::Label( this->tr( '1 (tbd)' ) ),       4, 1, Qt::AlignRight() );
+    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),       5, 1, Qt::AlignRight() );
+    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),       6, 1, Qt::AlignRight() );
 
     my $groupBox = Qt::GroupBox();
     $groupBox->setLayout( $table );
@@ -134,19 +135,17 @@ sub qualities {
     $table->addWidget( Qt::Label( this->tr( 'WIS:' ) ),        3, 0 );
     $table->addWidget( Qt::Label( this->tr( 'DEX:' ) ),        4, 0 );
     $table->addWidget( Qt::Label( this->tr( 'CON:' ) ),        5, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'CHR:' ) ),        6, 0 );
 
-    # empty space
+    # spacer
     $table->setColumnMinimumWidth( 1, 10 );
     $table->addWidget( Qt::Label( this->tr( 'Self' ) ),        0, 2 );
     $table->addWidget( Qt::Label( this->tr( '17 (tbd)' ) ),    1, 2 );
-    $table->addWidget( Qt::Label( this->tr( '12 (tbd)' ) ),    2, 2 );
-    $table->addWidget( Qt::Label( this->tr( '12 (tbd)' ) ),    3, 2 );
-    $table->addWidget( Qt::Label( this->tr( '17 (tbd)' ) ),    4, 2 );
-    $table->addWidget( Qt::Label( this->tr( '14 (tbd)' ) ),    5, 2 );
-    $table->addWidget( Qt::Label( this->tr( '10 (tbd)' ) ),    6, 2 );
+    $table->addWidget( Qt::Label( this->tr( '10 (tbd)' ) ),    2, 2 );
+    $table->addWidget( Qt::Label( this->tr( '10 (tbd)' ) ),    3, 2 );
+    $table->addWidget( Qt::Label( this->tr( '18 (tbd)' ) ),    4, 2 );
+    $table->addWidget( Qt::Label( this->tr( '10 (tbd)' ) ),    5, 2 );
 
-    # empty space
+    # spacer
     $table->setColumnMinimumWidth( 3, 10 );
     $table->addWidget( Qt::Label( this->tr( 'RB' ) ),          0, 4 );
     $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    1, 4 );
@@ -154,19 +153,17 @@ sub qualities {
     $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    3, 4 );
     $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    4, 4 );
     $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    5, 4 );
-    $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    6, 4 );
 
-    # empty space
+    # spacer
     $table->setColumnMinimumWidth( 5, 10 );
     $table->addWidget( Qt::Label( this->tr( 'CB' ) ),          0, 6 );
-    $table->addWidget( Qt::Label( this->tr( '+5 (tbd)' ) ),    1, 6 );
+    $table->addWidget( Qt::Label( this->tr( '+3 (tbd)' ) ),    1, 6 );
     $table->addWidget( Qt::Label( this->tr( '-2 (tbd)' ) ),    2, 6 );
     $table->addWidget( Qt::Label( this->tr( '-2 (tbd)' ) ),    3, 6 );
     $table->addWidget( Qt::Label( this->tr( '+2 (tbd)' ) ),    4, 6 );
     $table->addWidget( Qt::Label( this->tr( '+2 (tbd)' ) ),    5, 6 );
-    $table->addWidget( Qt::Label( this->tr( '-1 (tbd)' ) ),    6, 6 );
 
-    # empty space
+    # spacer
     $table->setColumnMinimumWidth( 7, 10 );
     $table->addWidget( Qt::Label( this->tr( 'EB' ) ),          0, 8 );
     $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    1, 8 );
@@ -174,17 +171,27 @@ sub qualities {
     $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    3, 8 );
     $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    4, 8 );
     $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    5, 8 );
-    $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),    6, 8 );
 
-    # empty space
+    # spacer
     $table->setColumnMinimumWidth( 9, 10 );
-    $table->addWidget( Qt::Label( this->tr( 'Self' ) ),        0, 10 );
-    $table->addWidget( Qt::Label( this->tr( '18/40 (tbd)' ) ), 1, 10 );
-    $table->addWidget( Qt::Label( this->tr( '10 (tbd)' ) ),    2, 10 );
-    $table->addWidget( Qt::Label( this->tr( '10 (tbd)' ) ),    3, 10 );
-    $table->addWidget( Qt::Label( this->tr( '18/10 (tbd)' ) ), 4, 10 );
-    $table->addWidget( Qt::Label( this->tr( '16 (tbd)' ) ),    5, 10 );
-    $table->addWidget( Qt::Label( this->tr( '9 (tbd)' ) ),     6, 10 );
+    $table->addWidget( Qt::Label( this->tr( 'Best' ) ),        0, 10 );
+    $table->addWidget( Qt::Label( this->tr( '18/20 (tbd)' ) ), 1, 10 );
+    $table->addWidget( Qt::Label( this->tr( '8 (tbd)' ) ),     2, 10 );
+    $table->addWidget( Qt::Label( this->tr( '8 (tbd)' ) ),     3, 10 );
+    $table->addWidget( Qt::Label( this->tr( '18/20 (tbd)' ) ), 4, 10 );
+    $table->addWidget( Qt::Label( this->tr( '12 (tbd)' ) ),    5, 10 );
+
+    # spacer
+    $table->setColumnMinimumWidth( 11, 10 );
+    $table->addWidget( Qt::Label( this->tr( 'Cost' ) ),        0, 12 );
+    $table->addWidget( Qt::Label( this->tr( '8 (tbd)' ) ),     1, 12 );
+    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),     2, 12 );
+    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),     3, 12 );
+    $table->addWidget( Qt::Label( this->tr( '12 (tbd)' ) ),    4, 12 );
+    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),     5, 12 );
+
+    $table->addWidget( Qt::Label( this->tr( 'Total Cost:' ) ), 6, 6, 1, 5, Qt::AlignRight() );
+    $table->addWidget( Qt::Label( this->tr( '20/20 (tbd)' ) ), 6, 12 );
 
     my $groupBox = Qt::GroupBox();
     $groupBox->setLayout( $table );
@@ -194,25 +201,25 @@ sub qualities {
 sub detailedOne {
     my $table = Qt::GridLayout();
 
-    $table->addWidget( Qt::Label( this->tr( 'Level' ) ),          0, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Cur Exp' ) ),        1, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Max Exp' ) ),        2, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Adv Exp' ) ),        3, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'MaxDepth' ) ),       4, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Game Turns' ) ),     5, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Standard Turns' ) ), 6, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Resting Turns' ) ),  7, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Gold' ) ),           8, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Level' ) ),         0, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Cur Exp' ) ),       1, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Max Exp' ) ),       2, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Adv Exp' ) ),       3, 0 );
+    $table->addWidget( Qt::Label( this->tr( '' ) ),              4, 0 ); # spaser
+    $table->addWidget( Qt::Label( this->tr( 'Gold' ) ),          5, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Burden' ) ),        6, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Speed' ) ),         7, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Max Depth' ) ),     8, 0 );
 
-    $table->addWidget( Qt::Label( this->tr( '1 (tbd)' ) ),    0, 1 );
-    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),    1, 1 );
-    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),    2, 1 );
-    $table->addWidget( Qt::Label( this->tr( '10 (tbd)' ) ),   3, 1 );
-    $table->addWidget( Qt::Label( this->tr( 'Town (tbd)' ) ), 4, 1 );
-    $table->addWidget( Qt::Label( this->tr( '1 (tbd)' ) ),    5, 1 );
-    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),    6, 1 );
-    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),    7, 1 );
-    $table->addWidget( Qt::Label( this->tr( '600 (tbd)' ) ),  8, 1 );
+    $table->addWidget( Qt::Label( this->tr( '1 (tbd)' ) ),       0, 1 );
+    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),       1, 1 );
+    $table->addWidget( Qt::Label( this->tr( '0 (tbd)' ) ),       2, 1 );
+    $table->addWidget( Qt::Label( this->tr( '10 (tbd)' ) ),      3, 1 );
+    # spacer
+    $table->addWidget( Qt::Label( this->tr( '600 (tbd)' ) ),     5, 1 );
+    $table->addWidget( Qt::Label( this->tr( '0.0 lbs (tbd)' ) ), 6, 1 );
+    $table->addWidget( Qt::Label( this->tr( 'Normal (tbd)' ) ),  7, 1 );
+    $table->addWidget( Qt::Label( this->tr( 'Town (tbd)' ) ),    8, 1 );
 
     my $groupBox = Qt::GroupBox();
     $groupBox->setLayout( $table );
@@ -222,25 +229,25 @@ sub detailedOne {
 sub detailedTwo {
     my $table = Qt::GridLayout();
 
-    $table->addWidget( Qt::Label( this->tr( 'Armor' ) ),  0, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Fight' ) ),  1, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Melee' ) ),  2, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Shoot' ) ),  3, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Blows' ) ),  4, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Shots' ) ),  5, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Infra' ) ),  6, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Speed' ) ),  7, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Burden' ) ), 8, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Armor' ) ),        0, 0 );
+    $table->addWidget( Qt::Label( this->tr( '' ) ),             1, 0 ); # spacer
+    $table->addWidget( Qt::Label( this->tr( 'Melee' ) ),        2, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'To-hit' ) ),       3, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Blows' ) ),        4, 0 );
+    $table->addWidget( Qt::Label( this->tr( '' ) ),             5, 0 ); # spaser
+    $table->addWidget( Qt::Label( this->tr( 'Shoot to-dam' ) ), 6, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'To-hit' ) ),       7, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Shots' ) ),        8, 0 );
 
     $table->addWidget( Qt::Label( this->tr( '[0, +2] (tbd)' ) ),  0, 1 );
-    $table->addWidget( Qt::Label( this->tr( '(+4, +3) (tbd)' ) ), 1, 1 );
-    $table->addWidget( Qt::Label( this->tr( '(+4, +3) (tbd)' ) ), 2, 1 );
-    $table->addWidget( Qt::Label( this->tr( '(+4, +0) (tbd)' ) ), 3, 1 );
-    $table->addWidget( Qt::Label( this->tr( '3.7/turn (tbd)' ) ), 4, 1 );
-    $table->addWidget( Qt::Label( this->tr( '0/turn (tbd)' ) ),   5, 1 );
-    $table->addWidget( Qt::Label( this->tr( '0 ft (tbd)' ) ),     6, 1 );
-    $table->addWidget( Qt::Label( this->tr( 'Normal (tbd)' ) ),   7, 1 );
-    $table->addWidget( Qt::Label( this->tr( '0.0lbs (tbd)' ) ),   8, 1 );
+    # spacer
+    $table->addWidget( Qt::Label( this->tr( '1d1, +3 (tbd)' ) ),  2, 1 );
+    $table->addWidget( Qt::Label( this->tr( '24, +4 (tbd)' ) ),   3, 1 );
+    $table->addWidget( Qt::Label( this->tr( '3.0/turn (tbd)' ) ), 4, 1 );
+    # spacer
+    $table->addWidget( Qt::Label( this->tr( '+0 (tbd)' ) ),       6, 1 );
+    $table->addWidget( Qt::Label( this->tr( '19, +4 (tbd)' ) ),   7, 1 );
+    $table->addWidget( Qt::Label( this->tr( '0/turn (tbd)' ) ),   8, 1 );
 
     my $groupBox = Qt::GroupBox();
     $groupBox->setLayout( $table );
@@ -250,24 +257,23 @@ sub detailedTwo {
 sub detailedThree {
     my $table = Qt::GridLayout();
 
-    $table->addWidget( Qt::Label( this->tr( 'Saving Throw' ) ),  0, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Stealth' ) ),       1, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Fighting' ) ),      2, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Shooting' ) ),      3, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Disarming' ) ),     4, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Magic Device' ) ),  5, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Perception' ) ),    6, 0 );
-    $table->addWidget( Qt::Label( this->tr( 'Searching' ) ),     7, 0 );
-    $table->addWidget( Qt::Label( this->tr( '' ) ),              8, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Saving Throw' ) ), 0, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Stealth' ) ),      1, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Disarming' ) ),    2, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Magic Device' ) ), 3, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Perception' ) ),   4, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Searching' ) ),    5, 0 );
+    $table->addWidget( Qt::Label( this->tr( 'Infravision' ) ),  6, 0 );
+    $table->addWidget( Qt::Label( this->tr( '' ) ),             7, 0 );
+    $table->addWidget( Qt::Label( this->tr( '' ) ),             8, 0 );
 
-    $table->addWidget( Qt::Label( this->tr( '20% (tbd)' ) ),       0, 1 );
-    $table->addWidget( Qt::Label( this->tr( 'Poor (tbd)' ) ),      1, 1 );
-    $table->addWidget( Qt::Label( this->tr( 'Very Good (tbd)' ) ), 2, 1 );
-    $table->addWidget( Qt::Label( this->tr( 'Fair (tbd)' ) ),      3, 1 );
-    $table->addWidget( Qt::Label( this->tr( '26% (tbd)' ) ),       4, 1 );
-    $table->addWidget( Qt::Label( this->tr( '19 (tbd)' ) ),        5, 1 );
-    $table->addWidget( Qt::Label( this->tr( '1 in 38 (tbd)' ) ),   6, 1 );
-    $table->addWidget( Qt::Label( this->tr( '14% (tbd)' ) ),       7, 1 );
+    $table->addWidget( Qt::Label( this->tr( '20% (tbd)' ) ),     0, 1 );
+    $table->addWidget( Qt::Label( this->tr( 'Bad (tbd)' ) ),     1, 1 );
+    $table->addWidget( Qt::Label( this->tr( '26% (tbd)' ) ),     2, 1 );
+    $table->addWidget( Qt::Label( this->tr( '19 (tbd)' ) ),      3, 1 );
+    $table->addWidget( Qt::Label( this->tr( '1 in 38 (tbd)' ) ), 4, 1 );
+    $table->addWidget( Qt::Label( this->tr( '14% (tbd)' ) ),     5, 1 );
+    $table->addWidget( Qt::Label( this->tr( '0 ft (tbd)' ) ),    6, 1 );
 
     my $groupBox = Qt::GroupBox();
     $groupBox->setLayout( $table );
